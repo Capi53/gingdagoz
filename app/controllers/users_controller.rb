66b552f61bc0fa975c8983e16e16624344e5d166
東條竜也 <tatsuya@Tatsuya.local>
2020-10-30 10:46:59 +0900

    sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,8 @@ class UsersController < ApplicationController
   
    def create
       @user = User.new(user_params)
-   
-      if @users.save
+    # debugger
+      if @user.save
         redirect_to @user
       else
         render 'new'
