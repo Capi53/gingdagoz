5a4c93c6ec48cb575d6e2079096a252080e4b0a4
Capi53 <e185753@ie.u-ryukyu.ac.jp>
2020-11-02 16:38:20 +0900

    add login routings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'homes/index'
   
@@ -8,4 +9,7 @@ Rails.application.routes.draw do
 
   get '/users/index'
   resources :users
-end
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
