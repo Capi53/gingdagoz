226339ea0dafb0a3fc0817b88353a86cb611a586
AzamA38 <e185731@ie.u-ryukyu.ac.jp>
2020-11-04 11:08:37 +0900

    たつやのpullしていろいろいじった

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'homes/index'
   
@@ -7,5 +8,14 @@ Rails.application.routes.draw do
   root 'homes#index'
 
   get '/users/index'
+
+  get 'signup' => 'users#new'
+
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
+
   resources :users
 end
