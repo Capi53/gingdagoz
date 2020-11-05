22c9c71b9928c1d76d2d43d51409f353c6326b1f
ぴょまえら <44591777+Capi53@users.noreply.github.com>
2020-11-05 16:02:51 +0900

    Update user.rb
    5~9行目追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   def show
     @member = User.find(params[:id])
   end  
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
