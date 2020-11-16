885281dc89f3d12e44b3bc67cd2a0f46db513a4a
Capi53 <e185753@ie.u-ryukyu.ac.jp>
2020-11-16 19:00:28 +0900

    develop開発

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,2 +1,5 @@
 class HomesController < ApplicationController
+    # def index
+    #     @user_name = User.find(params[:session])
+    # end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in @user
-      redirect_to @user
+      redirect_to root_path
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
