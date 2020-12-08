70eb0e46cccdec54e66c8a3a0c1e2a838676a346
AzamA38 <e185731@ie.u-ryukyu.ac.jp>
2020-12-08 16:30:45 +0900

    ta_indexをTAしか入れないようにした

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,6 @@
 class HomesController < ApplicationController
+    before_action :ta_required, :only => [:ta_index]
+
     def index
         @users = User.all
         #     @user_name = User.find(params[:session])
@@ -6,10 +8,18 @@ class HomesController < ApplicationController
     end
 
     def ta_index
-        binding.pry
         @users = User.all
         #     @user_name = User.find(params[:session])
         @events = Event.all
     end
-  
+
+    private
+    def ta_required
+        if current_user.role_id == 3
+            redirect_to users_index_path
+        elsif current_user.role_id == 1
+            redirect_to homes_index_path
+        end
+    end
+
 end
